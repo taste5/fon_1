bafc51cbdc6079ddd9d047d6831d7f039f764377
taste1 <ishmael_goony@mailbox.org>
2025-11-02 02:07:24 +0100

    implement sleek fsm

--- include/globals.h
+++ include/globals.h
@@ -5,12 +5,30 @@
 #include <Arduino.h>
 
 
-// ===== OSC settings =====
-WiFiUDP Udp;
+struct SystemData {
+  byte state;
+  byte prev_state;
+  byte event;
+  byte connectionStatus; 
+};
 
-Button pickup;
-SystemData data;
-Melody melody;
+
+
+enum States {
+  STATE_NOT_CONNECTED = 0x00,
+  STATE_IDLE = 0x01,
+  STATE_RINGING = 0x02,
+  STATE_PICKEDUP = 0x04
+};
+
+
+enum Events {
+  EVENT_NONE = 0x00,
+  EVENT_KEY_PRESSED = 0x01,
+  EVENT_INCOMING_MESSAGE = 0x02,
+  EVENT_PICKUP = 0x04,
+  EVENT_LOST_CONNECTION = 0x08,
+};
 
 
 #endif 
--- src/main.cpp
+++ src/main.cpp
@@ -2,141 +2,181 @@
 #include "config.h"
 #include "globals.h"
 #include "buttons.h"
-#include "chords.h"
-#include <WiFi.h>
+#include "ring.h"
+#include "secrets.h"
+#include "message.h"
+
+Melody melody;
+WiFiUDP Udp;
+Button pickup;
+SystemData MachineData;
+
+void blinkBuiltin(){
+  const int cycleTime = 500;
+  static unsigned long lastBlink=0;
+  if(millis() - lastBlink > cycleTime){digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));lastBlink = millis();}
+}
 
 
-
-// ===== WiFi settings =====
-const char* ssid     = "netzmork2G";
-const char* password = "floodzone";
-
-// ===== Access Point fallback =====
-const char* ap_ssid = "ESP32_OSC_AP";
-const char* ap_pass = "12345678";
-
-
-
-// Chord definitions
-
-
-struct Melody {
-  int noteDuration = 100; // ms per note in arpeggio
-  int currentNote = 0;
-  int currentChord =0;
-  int chordDelay = 400;   // pause after each chord
-  unsigned long lastTone;
-  bool in_pause = false;
-  int ring_twice = 0;
-
-};
-
-struct SystemData {
-  bool is_ringing = false;
-};
-
-
-bool playChord(const int chord[], Melody *m) {
-  const int size = 3;
-  if (m->currentNote >= size)
+void onExit(enum States s)
+{
+  switch (s)
   {
-    return false;
-  }
+  case STATE_NOT_CONNECTED:
+    digitalWrite(LED_BUILTIN,LOW);
+    break;
   
-  if ( millis() - m->lastTone > m->noteDuration)
+  default:
+    break;
+  }
+}
+
+void onEnter(enum States s)
+{
+  switch (s)
   {
-    noTone(BUZZER_PIN);
-    tone(BUZZER_PIN, chord[m->currentNote]);
-    m->currentNote++;
-    m->lastTone = millis();
-  }
-  return true;
-}
-void ring( Melody *m){
-  const int *chords[] = {chordC,chordF,chordG};
-    
-switch (m->in_pause)
-{
-case 0:
-if (m->currentChord >= 3)
-{
-  m->in_pause = true;
-  noTone(BUZZER_PIN);
-}
-
-if(!playChord(chords[m->currentChord],m)){
-    m->currentChord ++;
-    m->currentNote = 0;
- }
+  case STATE_NOT_CONNECTED:
+    digitalWrite(LED_BUILTIN, HIGH);
+    break;
   
-  break;
+  default:
+    break;
+  }
+}
 
-case 1:
-if (millis() - m->chordDelay > m->lastTone)
+void transitToState(enum States s)
 {
-    m->lastTone = millis();
-    m->in_pause = false;
-    m->currentChord = 0;
+  Serial.print("Transition from ");Serial.print(MachineData.state); Serial.print(" to ");Serial.println(s);
+  // osc.sendCurrentStatus();
+  onExit((enum States)MachineData.state);
+  onEnter(s);
+  MachineData.state = s;
 }
+void processEvent(){
+  byte event = EVENT_NONE;
+  byte counter = 0;
+  byte storage;
+
+  do{
+    storage =1<<(counter);  /* Maske zum erkennen von Bits erzeugen */
+    if ((storage&MachineData.event)!=0)
+    {
+      event = storage; //Event recognised, extract event
+      MachineData.event &=~storage; // clear from memory
+    }
+    counter++;
+  }while (
+    (counter < (sizeof(MachineData.event)*8))&&
+    (event == EVENT_NONE)
+  );
 
 
-default:
-  break;
+  switch (MachineData.state)
+  {
+    case STATE_NOT_CONNECTED:
+      MachineData.connectionStatus = connectToWifi(ssid,password,ap_ssid,ap_pass);
+      if (MachineData.connectionStatus == WL_CONNECTED)
+      {
+        transitToState(STATE_IDLE);
+      }
+      
+    break;
+    case STATE_IDLE:
+      switch (event)
+      {
+      case EVENT_INCOMING_MESSAGE:
+        // processIncomingMessage();
+        break;
+      
+      case EVENT_KEY_PRESSED:
+        // processKeyPressOnIdle;
+      break;
+
+      case EVENT_PICKUP:
+        // goToState(STATE_PICKUP); //
+      break;
+
+      case EVENT_LOST_CONNECTION:
+        transitToState(STATE_NOT_CONNECTED);
+      break;
+      default:
+      blinkBuiltin();
+        break;
+      }
+    break;
+      case STATE_RINGING:
+      switch (event)
+      {
+        case EVENT_INCOMING_MESSAGE:
+          // processIncomingMessage();
+        break;
+      
+        case EVENT_KEY_PRESSED:
+          // processKeyPressOnRing;
+        break;
+
+        case EVENT_PICKUP:
+          // goToState(STATE_PICKUP);
+        break;
+      
+        case EVENT_LOST_CONNECTION:
+          // goToState(STATE_NOT_CONNECTED);
+        break;
+
+        default:
+          // ring();
+        break;
+      }
+    break;
+    case STATE_PICKEDUP:
+       switch (event)
+      {
+        case EVENT_INCOMING_MESSAGE:
+          // processIncomingMessage();
+        break;
+      
+        case EVENT_KEY_PRESSED:
+          // processKeyPressOnPickup;
+        break;
+
+        case EVENT_PICKUP:
+          // goToState(STATE_IDLE);
+        break;
+      
+        case EVENT_LOST_CONNECTION:
+          // goToState(STATE_NOT_CONNECTED);
+        break;
+
+        default:
+        break;
+      }
+    break;
+      default:
+    break;
+  }
 }
- 
-}
-
-
-
-
-
 
 
 void setup(){
 
-  pinMode(PICKUP_PIN, INPUT_PULLUP);
-  pickup.pin = PICKUP_PIN;
-  pickup.state = digitalRead(pickup.pin);
-  pickup.lastDbT = millis();
-  pickup.id = 0;
-  data.is_ringing = true;
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  btnInit(&pickup,PICKUP_PIN, 0);
   Serial.begin(9600);
-    delay(1000);
+  delay(100);
 
+  transitToState(STATE_NOT_CONNECTED);
 
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
+  
 
-  unsigned long startAttemptTime = millis();
-
-  // Try to connect for 10 seconds
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("\nConnected to WiFi!");
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-  } else {
-    Serial.println("\nFailed to connect. Starting Access Point...");
-    WiFi.softAP(ap_ssid, ap_pass);
-    Serial.print("AP IP Address: ");
-    Serial.println(WiFi.softAPIP());
-  }
-
-  Udp.begin(LOCAL_PORT);
-  Serial.printf("Listening for OSC on port %d\n", LOCAL_PORT);
-};
-
-
-
+}
 
 void loop() {
-
-    // Receive OSC messages
+  if (WiFi.status() == WL_CONNECTION_LOST)
+  {
+    MachineData.event = EVENT_LOST_CONNECTION;
+  }
+  
+  // Receive OSC messages
   OSCMessage msgIN;
   int size = Udp.parsePacket();
   if (size > 0) {
@@ -144,15 +184,15 @@ void loop() {
       msgIN.fill(Udp.read());
     }
     if (!msgIN.hasError()) {
-      msgIN.dispatch("/test", handleTest);
-      msgIN.dispatch("/ring", handleRing);
+      // msgIN.dispatch("/test", handleTest);
+      // msgIN.dispatch("/ring", handleRing);
     }
   }
 
   // Example: Send an OSC message every 5 seconds
   static unsigned long lastSend = 0;
   if (millis() - lastSend > 5000) {
-    sendHeartbeat();
+    // sendHeartbeat();
     lastSend = millis();
   }
 
@@ -166,18 +206,16 @@ void loop() {
     case BTN_PRESSED:
     break;
     case BTN_RELEASED:
-    data.is_ringing = false;
     noTone(BUZZER_PIN);
     break;
     default:
     break;
   }
-  if (data.is_ringing)
-  {
-    ring(&melody);
-  }
-  
 
+
+
+  processEvent();
+  
 
 
 }
