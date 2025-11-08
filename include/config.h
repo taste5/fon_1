f79b990afce375e4a76c9fa57cebb5ed50200388
taste1 <ishmael_goony@mailbox.org>
2025-11-08 01:26:23 +0100

    Added New HW Pins

--- include/config.h
+++ include/config.h
@@ -2,8 +2,18 @@
 #define __CONFIG_H__
 
 
-#define BUZZER_PIN 13
-#define PICKUP_PIN 15
+#define BUZZER_PIN 18
+#define PICKUP_PIN 19
+
+#define KEY_PIN_COL_0 33
+#define KEY_PIN_COL_1 25
+#define KEY_PIN_COL_2 26
+
+#define KEY_PIN_ROW_0 27
+#define KEY_PIN_ROW_1 14
+#define KEY_PIN_ROW_2 12
+#define KEY_PIN_ROW_3 13
+
 
 #define REMOTE_IP "192.168.8.133" 
 #define LOCAL_PORT 9000    // Port to listen for incoming OSC
