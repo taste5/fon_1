244b96328c48df566845c0bbdd859a5309f09f1d
taste1 <ishmael_goony@mailbox.org>
2025-11-25 20:46:05 +0100

    update pin config, and add constants

--- include/config.h
+++ include/config.h
@@ -2,8 +2,10 @@
 #define __CONFIG_H__
 
 
-#define BUZZER_PIN 18
-#define PICKUP_PIN 19
+#define BUZZER_PIN 23
+#define PICKUP_PIN 22
+
+
 
 #define KEY_PIN_COL_0 33
 #define KEY_PIN_COL_1 25
@@ -14,8 +16,15 @@
 #define KEY_PIN_ROW_2 12
 #define KEY_PIN_ROW_3 13
 
+#define MODIFIER_KEY_LEFT 4
+#define MODIFIER_KEY_RIGHT 16
+#define MODIFIER_CNT 2
+
 
 #define REMOTE_IP "192.168.8.133" 
 #define LOCAL_PORT 9000    // Port to listen for incoming OSC
 #define REMOTE_PORT 9001 // Remote host to send OSC
+
+#define TIMER_PRESCALER 80
+
 #endif 
