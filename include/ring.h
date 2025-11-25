e702e14bebc45b421f96791076a27092359e5e1e
taste1 <ishmael_goony@mailbox.org>
2025-11-25 20:47:53 +0100

    minor refactor

--- include/ring.h
+++ include/ring.h
@@ -6,10 +6,11 @@
 #include "globals.h"
 #include "config.h"
 
- const int chordC[3] = {523, 659, 784};     // C major
-  const int chordF[3] = {698, 880, 1046};    // F major
-  const int chordG[3] = {784, 988, 1175};    // G major
-  const unsigned long ringPattern[] = { 170, 170, 460 }; // ms durations
+
+const int chordC[3] = {523, 659, 784};     // C major
+const int chordF[3] = {698, 880, 1046};    // F major
+const int chordG[3] = {784, 988, 1175};    // G major
+const unsigned long ringPattern[] = { 170, 170, 460 }; // ms durations
 
 struct Melody {
   int noteDuration = 100; // ms per note in arpeggio
@@ -19,6 +20,7 @@ struct Melody {
   unsigned long lastTone;
   bool in_pause = false;
   int ring_twice = 0;
-
+  
 };
+void ring( Melody *m);
 #endif 
